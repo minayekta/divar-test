7a5c73ebb2aaa2174ba1fa7acac845e570a05b5e
minayekta <mina.qolizadeh@hotmail.com>
2024-06-05 09:37:08 -0700

    add axios and add files for otp handle form

---
+++ src/components/templates/SendOtpFrom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SendOtpFrom() {
+  return (
+    <div>SendOtpFrom</div>
+  )
+}
+
+export default SendOtpFrom
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import SendOtpFrom from '../components/templates/SendOtpFrom'
+import CheckOtpForm from '../components/templates/CheckOtpForm'
+
+function AuthPage() {
+  const [step, setStep] = useState(1)
+  const [mobile, setMobile] = useState('')
+  const [code, setCode] = useState('')
+  return (
+    <div>
+      {step === 1 && <SendOtpFrom />}
+      {step === 2 && <CheckOtpForm />}
+    </div>
+  )
+}
+
+export default AuthPage
